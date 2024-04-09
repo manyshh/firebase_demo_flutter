1b91ecb5ced6b4935c4c0441c911a626cf783369
manish.shukla <manish.shukla@ideainfinityit.com>
2024-04-09 15:38:43 +0530

    removed code

--- lib/utils/string_app.dart
+++ lib/utils/string_app.dart
@@ -5,40 +5,10 @@ class String_App {
   String enterMobileNumber = "Enter your mobile number";
   String enterUserName = "Enter Username";
   String enterPassword = "Enter Password";
-
-  String username = "User Name";
-  String password = "Password";
   String mobileRegister = "Register Mobile";
   String signIn = "Sign In";
 
-  String login_00 = 'Please Enter Your Registered User Name';
-  String loginmessage = 'Please Enter the Correct User Name';
-  String login_01 = 'Please Enter A Valid Login Name';
-  String login_02 = 'Please Enter The Password';
-  String login_03 = 'Password Must Contain At Least 8 Characters';
-  String login_04 = 'Sign-In';
-  String UPDATE = 'UPDATE';
-  String Register = 'Register';
-  String Proceed = 'Proceed';
-  String login_05 = 'Enter Mobile Number Here';
-  String login_13 = '192';
-  String login_14 = '202';
-  String login_15 = '210';
-  String login_16 = '109';
-  String login_17 = '3306';
-  String login_18 = 'database name';
-  String login_19 = 'username';
-  String login_20 = 'password';
-
-  String login_06 = 'Mobile Number';
-  String login_07 = 'Enter Password';
-  String login_08 = 'Password';
   String forgotPassword = 'Forgot Password?';
-  String login_10 = 'Sign-Up';
-  String ApkUpdate = 'ApkUpdate';
-  String login_11 = "Don't Have An Account?";
-  String login_12 = 'Sign-In Via OTP';
-  String phoneno = "Please Enter Your Registered Phone Number We Will Send You Confirmation Code.";
 
 //#endregion
 }
